8c6043c332efd7cf53b1e3472e5ee122f7749ce9
zhengshibin <zhengshibin@xueqiu.com>
2022-10-27 14:59:08 +0800

    feat: 增加阴影和灯光动画

--- src/index.js
+++ src/index.js
@@ -1,23 +1,44 @@
-import * as THREE from 'three';
+import * as T from 'three';
 
-// init
+const scene = new T.Scene();
+const camera = new T.PerspectiveCamera( 25, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new T.WebGLRenderer();
 
-const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-camera.position.z = 1;
-
-const scene = new THREE.Scene();
-
-const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-const material = new THREE.MeshNormalMaterial();
-
-const mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
-
-scene.add(new THREE.AxesHelper(5));
-
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setClearColor( new T.Color( 0xffffff ) );
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.shadowMap.enabled = true;
+
+const BoxGeo = new T.BoxGeometry( 1, 1, 1 );
+const BoxMat = new T.MeshLambertMaterial( { color: 0x329CEE } );
+const Box = new T.Mesh( BoxGeo, BoxMat );
+Box.castShadow = true;
+Box.position.set( 4, 4, 4 );
+
+const PlaneGeo = new T.PlaneGeometry( 50, 50,1,1 );
+const PlaneMat = new T.MeshLambertMaterial( { color: 0xEC6968 } );
+const Plane = new T.Mesh( PlaneGeo, PlaneMat );
+Plane.receiveShadow = true;
+Plane.rotation.x = -0.5 * Math.PI;
+Plane.position.set( 15, 0, 0 );
+
+scene.add( Box );
+scene.add( Plane );
+
+camera.position.set( -30, 40, 30 );
+camera.lookAt( Box.position );
+
+const spotLight = new T.SpotLight( 0xffffff );
+spotLight.position.set( 10, 20, -5 );
+spotLight.castShadow = true;
+scene.add( spotLight );
+
+// scene.add(new T.SpotLightHelper(spotLight));
+scene.add( new T.AxesHelper( 20 ) );
+
+renderer.render( scene, camera );
+
 renderer.setAnimationLoop( animation );
+
 document.body.appendChild( renderer.domElement );
 
 
@@ -25,8 +46,7 @@ document.body.appendChild( renderer.domElement );
 
 function animation( time ) {
 
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
+	spotLight.position.set(Math.sin(time/1000)*20, 40, Math.cos(time/1000)*20);
 
 	renderer.render( scene, camera );
 
